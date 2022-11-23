a08911176379ff8009a27f5b6d20e07054981d2b
corno <corno.astn.a@schraverus.com>
2022-11-23 18:47:13 +0100

    .

--- test_old/dist/implementation/private/createLoggingTypedHandler.p.d.ts
+++
@@ -1,4 +0,0 @@
-import * as th from "astn-typedhandlers-api";
-declare type TV = ["onEntry", null] | ["onDictionaryClose", null] | ["onDictionary", null] | ["onGroup", null] | ["onList", null] | ["onMultilineString", null] | ["onSimpleString", null] | ["onTaggedUnion", null] | ["onTypeReference", null] | ["onProperty", null] | ["onUnexpectedProperty", null] | ["onGroupClose", null] | ["onElement", null] | ["onListClose", null] | ["onOption", null] | ["onUnexpectedOption", null] | ["onTaggedUnionEnd", null];
-export declare function createLoggingTypedHandler<PAnnotation>(log: (tv: TV) => void): th.ITypedHandler<PAnnotation>;
-export {};
