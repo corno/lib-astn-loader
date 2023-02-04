e39c6651f7d5c8da4e1bf041eeb7d0c6298ba83f
corno <corno.astn.a@schraverus.com>
2023-02-04 20:02:22 +0100

    .

---
+++ pareto/src/data/project.data.ts
@@ -0,0 +1,27 @@
+import * as pr from 'pareto-core-raw'
+
+import * as mproject from "lib-pareto-typescript-project/dist/submodules/project"
+
+const d = pr.wrapRawDictionary
+
+import { $ as api } from "./api.data"
+
+export const $: mproject.TProject = {
+    'author': "Corno",
+    'description': "loads an ASTN Document from the filesystem",
+    'license': "ISC",
+
+    'pubdependencies': d({
+    }),
+    'type': ['library', {
+        'main': {
+            'definition': api,
+        },
+        'submodules': d({
+        }),
+        'test': {
+            'dependencies': d({
+            }),
+        }
+    }],
+}
